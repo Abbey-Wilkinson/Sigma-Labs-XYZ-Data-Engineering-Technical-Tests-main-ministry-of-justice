76dd883226f015b23b8f950264d071264910ce9d
Abbey Wilkinson <trainee.abbey.wilkinson@sigmalabs.co.uk>
2023-12-01 13:31:25 +0000

    Completed test_3

--- test_3.py
+++ test_3.py
@@ -7,4 +7,28 @@
 def sum_current_time(time_str: str) -> int:
     """Expects data in the format HH:MM:SS"""
     list_of_nums = time_str.split(":")
-    return sum(list_of_nums)
+
+    if len(list_of_nums) != 3:
+        return "Invalid number in list."
+
+    list_of_int_nums = []
+
+    for num in list_of_nums:
+
+        try:
+            list_of_int_nums.append(int(num))
+        except ValueError:
+            return "Error! Incorrect time_str format, must only contain integers and :"
+
+    for num in list_of_nums:
+
+        if len(num) != 1 or len(num) != 2:
+            return "Error, number is too long. Incorrect format."
+
+    print(list_of_int_nums)
+    return sum(list_of_int_nums)
+
+
+if __name__ == "__main__":
+
+    print(sum_current_time("01:0222:03"))
